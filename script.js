93adb29bc6a55facbd1bcc19a2dd787e5e14d167
Carrie Mah <mah.carrie@gmail.com>
2015-11-05 21:55:11 -0700

    Set up initial template

---
+++ script.js
@@ -0,0 +1,89 @@
+// Timer progress bar
+
+var timer = 0,
+    perc = 0,
+    timeTotal = 3000,
+    timeCount = 1,
+    cFlag;
+
+function updateProgress(percentage) {
+    var x = (percentage/timeTotal)*100,
+        y = x.toFixed(1);
+    $('#pbar_innerdiv').css("width", x + "%");
+    $('#pbar_innertext').text(y + "%");
+}
+
+function animateUpdate() {
+    if(perc < timeTotal) {
+        perc++;
+        updateProgress(perc);
+        timer = setTimeout(animateUpdate, timeCount);
+    } else if (perc == timeTotal) {
+        $('#pbar_donetext').css("display", "block");
+    }
+}
+
+// Interactions logic
+// If ing-amount = value
+// change the content
+// then make pop up visible
+
+// If ing-type = value
+// change content + image
+// make pop up visible
+// if click onto substitues li
+// change ing-type value = li value
+
+function popup(type) {
+    if (type = '0') {
+
+    } else if (type = '1') {
+
+    }
+}
+
+function amountUpdate(event) {
+
+    if ($("#ingredients td:contains('2 jars')")) {
+        $("#ing-1").css({top: 830, left: 20});
+        $("#ing-1").toggle();
+    }
+}
+
+function typeUpdate() {
+    // $("#ingredients td:contains('spaghetti sauce')").html("");
+    if ($("#ingredients td:contains('spaghetti sauce')")) {
+        $("#sub-1").css({top: 750, left: 80});
+        $("#sub-1").toggle();
+        // TODO click li, change img src=""
+    }
+
+    
+}
+
+$(document).ready(function() {
+    $("#pbar_outerdiv").click(function() {
+        if (cFlag == undefined) {
+            clearTimeout(timer);
+            perc = 0;
+            cFlag = true;
+            animateUpdate();
+        }
+        else if (!cFlag) {
+            cFlag = true;
+            animateUpdate();
+        }
+        else {
+            clearTimeout(timer);
+            cFlag = false;
+        }
+    });
+
+    $("#ing-amount").click(function(event) {
+        amountUpdate(event);
+    });
+
+    $("#ing-type").click(function() {
+        typeUpdate();
+    });
+}); 
